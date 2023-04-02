ecffa9403c7eb5b5848932d8ad98c064780cd816
Donald Winkelman <dmwink.tx@gmail.com>
2023-04-01 22:39:57 -0400

    First push with react and firebase working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBv9Oy9hhECYxkTlZO5Sa-dylL7I5BDp1w",
+  authDomain: "leetcollab-e2299.firebaseapp.com",
+  databaseURL: "https://leetcollab-e2299-default-rtdb.firebaseio.com",
+  projectId: "leetcollab-e2299",
+  storageBucket: "leetcollab-e2299.appspot.com",
+  messagingSenderId: "710099635216",
+  appId: "1:710099635216:web:d93abfc3e4e9dad3248e95",
+  measurementId: "G-4970DNSTCT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
